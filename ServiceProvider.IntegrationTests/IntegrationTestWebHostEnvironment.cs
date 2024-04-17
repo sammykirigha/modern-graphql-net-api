d981e624a2d08aef374bc7be9139139de8a67e72
sammykirigha <dkirigha18@gmail.com>
2024-04-18 00:52:04 +0300

    adding tests setup

---
+++ ServiceProvider.IntegrationTests/IntegrationTestWebHostEnvironment.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics.Eventing.Reader;
+using System.Runtime.CompilerServices;
+
+namespace ServiceProvider.IntegrationTests
+{
+	// hack to cope with new IWebHostEnvironment dependency in UserProfileService to create dev user
+	public class IntegrationTestWebHostEnvironment : IWebHostEnvironment
+	{
+		string IWebHostEnvironment.WebRootPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		IFileProvider IWebHostEnvironment.WebRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		string IHostEnvironment.ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		IFileProvider IHostEnvironment.ContentRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		string IHostEnvironment.ContentRootPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		string IHostEnvironment.EnvironmentName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+		private readonly bool isDevelopment = true;
+		public bool IsDevelopment() => isDevelopment;
+
+		private readonly bool isStaging = true;
+		public bool IsStaging() => isStaging;
+	}
+}
